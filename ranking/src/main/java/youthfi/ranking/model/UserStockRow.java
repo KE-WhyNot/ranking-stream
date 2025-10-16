3e6e00a4a69a640d3ed6266f6b9daba276e3d6f1
dayoung <dayoungann@naver.com>
2025-10-16 21:26:28 +0900

    [FIX] snake

--- ranking/src/main/java/youthfi/ranking/model/UserStockRow.java
+++ ranking/src/main/java/youthfi/ranking/model/UserStockRow.java
@@ -1,14 +1,27 @@
 package youthfi.ranking.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserStockRow {
+    @JsonProperty("user_id")
     private String userId;
+
+    @JsonProperty("stock_id")
     private String stockId;
+
+    @JsonProperty("holding_quantity")
     private int holdingQuantity;
+
+    @JsonProperty("avg_price")
     private double avgPrice;
 }
